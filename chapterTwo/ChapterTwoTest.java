e3fc1dcfb6e909dd5ac4fa8283b7c678f83d62b2
Reese <reeselevine@yahoo.com>
2015-11-16 19:29:08 +1100

    Solve 2.3 and 2.4
    Might have been better to do 2.4 recursively, since it gets a bit messy
    with an iterative approach.

--- chapterTwo/ChapterTwoTest.java
+++ chapterTwo/ChapterTwoTest.java
@@ -32,8 +32,57 @@ public class ChapterTwoTest {
         Node a = new Node(1);
         a.appendToTail(2); a.appendToTail(3);
         a.appendToTail(4); a.appendToTail(5);
-        assertEquals(3, a.findNode(2).data);
-        assertEquals(null, a.findNode(5));
-        assertEquals(5, a.findNode(0).data);
+        assertEquals(4, a.findNode(2).data);
+        assertEquals(null, a.findNode(6));
+        assertEquals(1, a.findNode(5).data);
+        assertEquals(5, a.findNode(1).data);
+    }
+
+    @Test
+    public void testDeleteNode() {
+        Node a = new Node(1);
+        a.appendToTail(3);
+        a.appendToTail(4);
+        Node.deleteNode(a.next);
+        assertEquals(4, a.next.data);
+        Node.deleteNode(a.next);
+        assertEquals(4, a.next.data);
+    }
+
+    @Test
+    public void testAddLists() {
+        Node a = new Node(3);
+        a.appendToTail(2);
+        Node b = new Node(4);
+        b.appendToTail(5);
+        Node c = new Node(7);
+        c.appendToTail(7);
+        Node d = Node.addLists(a, b);
+        assertEquals(c.data, d.data);
+        assertEquals(c.next.data, d.next.data);
+
+        c = new Node(1);
+        c.appendToTail(3);
+        c.appendToTail(1);
+        Node e = Node.addLists(b, d);
+        assertEquals(c.data, e.data);
+        assertEquals(c.next.data, e.next.data);
+        assertEquals(c.next.next.data, e.next.next.data);
+
+        a = new Node(7);
+        b = new Node(8);
+        b.appendToTail(9);
+        b.appendToTail(4);
+        c = new Node(5);
+        c.appendToTail(0);
+        c.appendToTail(5);
+        d = Node.addLists(a, b);
+        assertEquals(c.next.data, d.next.data);
+        d = Node.addLists(b, a);
+        assertEquals(c.next.next.data, d.next.next.data);
+
+        a = null;
+        c = Node.addLists(a, b);
+        assertEquals(b.data, c.data);
     }
 }
--- chapterTwo/Node.java
+++ chapterTwo/Node.java
@@ -46,18 +46,63 @@ public class Node {
      *  assuming that this node is the start of the list.
      */
     public Node findNode(int i) {
-        int count = 0;
-        Node n = this;
-        while (n.next != null) {
-            n = n.next;
-            count++;
+        if (i == 0) return null;
+        Node a = this;
+        Node b = this;
+        for (int j = i - 1; j > 0; j--) {
+            a = a.next;
+            if (a == null) return null;
         }
-        if (count - i < 0) return null;
-        n = this;
-        while (count - i > 0) {
-            n = n.next;
-            count--;
+        while (a.next != null) {
+            a = a.next;
+            b = b.next;
         }
-        return n;
+        return b;
+    }
+
+    /** Deletes the given node from the linked list. 
+     * Is unsolvable in the case that the given node is
+     * the last node in the linked list.
+     */
+    public static void deleteNode(Node node) {
+        if (node == null || node.next == null) return;
+        node.data = node.next.data;
+        node.next = node.next.next;
+    }
+
+    /** Adds together two linked lists, where the ones digit
+     *  is the head of the list, the tens digit is the next, etc.
+     *  Returns the head of the summed list.
+     *  Does not support negative numbers
+     */
+    public static Node addLists(Node l1, Node l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        int carry = (l1.data + l2.data) / 10;
+        Node result = new Node((l1.data + l2.data) % 10);
+        Node toReturn = result;
+        while (l1.next != null && l2.next != null) {
+            result.next = new Node((carry + l1.next.data + l2.next.data) % 10);
+            carry = (carry + l1.next.data + l2.next.data) / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+            result = result.next;
+        }
+        while (l1.next != null) {
+            result.next = new Node((carry + l1.next.data) % 10);
+            carry = (carry + l1.next.data) / 10;
+            result = result.next;
+            l1 = l1.next;
+        }
+        while (l2.next != null) {
+            result.next = new Node((carry + l2.next.data) % 10);
+            carry = (carry + l2.next.data) / 10;
+            result = result.next;
+            l2 = l2.next;
+        }
+        if (carry != 0) {
+            result.next = new Node(carry);
+        }
+        return toReturn;
     }
 }
